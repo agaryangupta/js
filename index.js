9f807e91cba09452f15a3bd0a5318a0fbed91ebe
agaryangupta <agaryangupta123@gmail.com>
2024-12-23 15:42:22 +0530

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+var mycollection=[
+    1,
+    "Hello World",
+    true,
+    {name:"rahul",age:30},
+    function yello(){
+        console.log("hellnah");
+    },
+    ["Anya","Forger","Loid","Forger"]
+];
+console.log(mycollection);
